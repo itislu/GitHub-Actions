9c75a569776442fac8d0b798a2f40188d0c121c3
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-07-14 05:11:27 +0200

    test: Add more format errors

--- testfiles/pull_request-comment/contact.tsx
+++ testfiles/pull_request-comment/contact.tsx
@@ -91,6 +91,10 @@ export async function action({ request }: ActionFunctionArgs) {
 export default function Contact() {
     const data = useLoaderData<SessionData>();
 
+
+
+
+    
     const contactFetcher = useFetcher<{
         errors?: { contactWay?: string; contactDetail?: string; message?: string; discordError?: string };
         success?: boolean;
